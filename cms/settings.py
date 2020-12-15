ac521b888ba5cb2ba1cd10b1100c1240421bb540
Osas Michael <macmive@gmail.com>
2020-12-15 01:19:32 +0000

    modified settings

--- cms/settings.py
+++ cms/settings.py
@@ -178,12 +178,14 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'cms/static')
 ]
 
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'cms/static/img')
 
 """
 S3 BUCKETS 
